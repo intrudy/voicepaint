443efea43e1490df7d458fd061b7922ec93cce6f
Harish Navnit <harishnavnit@gmail.com>
2024-09-05 23:13:58 +0800

    Add content scripts

---
+++ scripts/content.js
@@ -0,0 +1,37 @@
+
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var recognition = new SpeechRecognition()
+recognition.lang = 'en-US'
+recognition.continuous = false
+recognition.maxAlternatives = 1
+recognition.interimResults = false
+
+var msgBtn = document.querySelector('div[aria-label="Send Message"]')
+msgBtn.onclick = function (event) {
+    console.log("Send message!")
+    console.log(JSON.stringify(event, null, 2))
+}
+
+document.body.onclick = function() {
+    recognition.start()
+}
+
+recognition.onerror = function(event) {
+    console.log(`Speech recognition error detected: ${event.error}`)
+    console.log(`Additional information: ${event.message}`)
+}
+
+recognition.onspeechend = function() {
+    recognition.stop()
+}
+
+recognition.onresult = function(event) {
+    console.log('Color: ' + event.results[0][0].transcript)
+    console.log('Confidence: ' + event.results[0][0].confidence)
+
+    var textarea = document.getElementById(":rl:")
+    textarea.innerText = event.results[0][0].transcript
+}
