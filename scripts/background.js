38bf0e9cd74774a2d2632f85ad25cdf1a6411a73
Harish Navnit <harishnavnit@gmail.com>
2024-09-07 11:27:51 +0800

    remove custom command

--- scripts/background.js
+++ scripts/background.js
@@ -1,20 +1,6 @@
 
-async function speak() {
-    const [activeTab] = await chrome.tabs.query({ active: true, currentWindow: true })
-    chrome.tabs.sendMessage(activeTab.id, { command: "speak" })
-}
-
-chrome.action.onClicked.addListener(() => {
-    speak()
-})
-
-chrome.commands.onCommand.addListener((cmd) => {
-    if (cmd === 'speak') {
-        speak()
-    }
-})
-
 /**
+
 accelerometer=(),
 attribution-reporting=(),
 autoplay=(),
