9eef3f915e7dd2ddecb6ae7376eeddf31e7b7dce
Harish Navnit <harishnavnit@gmail.com>
2024-09-23 11:23:49 +0800

    try and type in the voice prompt

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,20 @@
 
+function keywrite(textarea, transcript, delay=100) {
+    let i = 0
+    textarea.autofocus = true
+
+    const interval = setInterval(() => {
+        if (i < transcript.length) {
+            textarea.value += transcript[i]
+            textarea.dispatchEvent(new KeyboardEvent('keypress', { bubbles: true }))
+            i++;
+        } else {
+            clearInterval(interval)
+            textarea.dispatchEvent(new Event('change', { bubbles: true }))
+        }
+    }, delay)
+}
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.error) {
         console.error("Runtime error: ", request.error)
@@ -9,7 +25,14 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     const textarea = document.querySelector('textarea[placeholder="Ask Meta AI anything..."')
 
     if (request.action === "write") {
-        textarea.innerText = request.data || textarea.textContent
+        keywrite(textarea, request.data || textarea.textContent)
+
+        msgBtn.style.pointerEvents = 'auto'
+        msgBtn.removeAttribute('disabled')
+        msgBtn.removeAttribute('aria-disabled')
+        msgBtn.setAttribute('tabindex', '0')
+        msgBtn.setAttribute('autofocus', true)
+        // msgBtn.setAttribute('aria-disabled', 'false')
     }
 
     if (request.action === "paint") {
